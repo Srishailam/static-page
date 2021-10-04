72e5d2d5adb9f015a23c3d4576002171aa5b8b01
Srishailam Dasari <sriyd531@gmail.com>
2021-10-04 00:03:17 -0400

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
+const FooterInfo = () => {
+  return <div className="FooterInfo">
+    <p>Check the background of our professionals on FINRA's on BrokerCheck</p>
+    
+  </div>
+}
+const ContactInfo = () => {
+  return <div className="ContactInfo">
+    <div>Need Help?</div>
+    <p>The Member Care Service Team is here always for whatever you need.</p>
+    <p>Phone: (123) 456-7896</p>
+    <p>Weekdays 8AM - 5 PM CST</p>
+  </div>
+}
+const HelpLinks = () => {
+  return <div className="HelpLinks">
+    <ul>
+      <li>
+        Terms & Conditions
+      </li>
+      <li>
+        Privacy Policy
+      </li>
+      <li>
+        All Help & Support
+      </li>
+    </ul>
+  </div>
+}
 export const Footer = () => {
   return (
     <div className="Footer">
-      Footer
+      <div className="FooterContent">
+        <Link exact to=""><img src="//logo.clearbit.com/spotify.com?size=30&greyscale=true" /></Link>
+        <HelpLinks />
+        <ContactInfo />
+        <FooterInfo />
+      </div>
     </div>
   )
 }
